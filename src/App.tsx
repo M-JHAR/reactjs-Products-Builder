40269b8eafcb6a523f0b87a715da7781dbc30013
Mustafa <jharmostafa@gmail.com>
2025-02-17 23:17:29 +0300

    Enhance input, button, and product modal form

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,9 @@ const App = ()=>
   const renderFormInputList = formInputsList.map( input =>
   (
     <div className="flex flex-col">
-      <label htmlFor={input.id}> {input.label} </label>
+      <label htmlFor={input.id} className="mb-[2px] text-md font-medium text-gray-700">
+        {input.label}
+      </label>
       <Input type={input.type} id={input.id} name={input.name}/>
     </div>
   ));
@@ -38,11 +40,14 @@ const App = ()=>
           </div>
 
           <Modal isOpen={isOpen} closeModal={closeModal} title={"ADD NEW PRODUCT"}>
-            {renderFormInputList}
-            <div className="flex items-center space-x-2">
+            <form className="space-y-3">
+              {renderFormInputList}
+              <div className="flex items-center space-x-3">
               <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
               <Button className="bg-gray-400 hover:bg-gray-500">Cancel</Button>
             </div>
+            </form>
+            
           </Modal>
         </main>
         
